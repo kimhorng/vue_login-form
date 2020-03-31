5c4b09198087b2718b4ebdd9919d12b9a2359411
unknown <hornkimhorng18@kit.edu.kh>
2020-03-31 22:39:29 +0700

    init files

---
+++ login/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-content style="background-color: red">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  components:{
+  },
+  data: () => ({
+  }),
+};
+</script>
---
+++ login/src/views/Success.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Successfull.....</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
